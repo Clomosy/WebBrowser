ca07f9849bb55bcd403078db1fd21d8fa1d4fc6d
ClomosyDev <134625867+Clomosy@users.noreply.github.com>
2023-06-05 10:47:42 +0300

    Create webbrowser.pas

---
+++ webbrowser.pas
@@ -0,0 +1,16 @@
+Var 
+  MyForm:TCLForm;
+  xWeb:TclWebBrowser;
+  ustBar:TCLProEdit;
+Begin
+  MyForm := TCLForm.Create(Self);
+  ustBar := MyForm.AddNewProEdit(MyForm,'ustBar','www.Clomosy.com');
+  clComponent.SetupComponent(ustBar,'{"Align" : "Top","Width" :180, "Height":45,"RoundHeight":10,"RoundWidth":10,"BorderColor":"#000000",
+  "BorderWidth":2}');
+  ustBar.Text := 'www.Clomosy.com';
+  ustBar.ReadOnly := True;
+  xWeb:= MyForm.AddNewWebBrowser(MyForm,'xWeb');  
+  xWeb.Align := alClient;
+  xWeb.Navigate('www.Clomosy.com');
+  MyForm.Run;
+End;
